302b7aab2168449b1ad6164a9f68ab4a22030ffb
Berthonise Charles <berthonisecharles@Berthonises-MacBook-Air-2.local>
2022-05-09 07:54:16 -0400

    1st semi completed project commit

---
+++ members/views.py
@@ -0,0 +1,11 @@
+from re import template
+from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+# Create your views here.
+
+class UserRegisterView(generic.CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
---
+++ thewally/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ThewallyConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'thewally'
